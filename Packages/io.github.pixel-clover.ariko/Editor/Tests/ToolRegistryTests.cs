7812ff7df8761184c5a042a559d8bb55be3fb5f9
Hassan Abedi <hassan.abedi.t@gmail.com>
2025-09-20 20:30:10 +0200

    WIP

--- Packages/io.github.pixel-clover.ariko/Editor/Tests/ToolRegistryTests.cs
+++ Packages/io.github.pixel-clover.ariko/Editor/Tests/ToolRegistryTests.cs
@@ -21,11 +21,11 @@ public class ToolRegistryTests
         var toolRegistry = new ToolRegistry(settings, "Agent");
 
         // Assert
-        Assert.IsNotNull(toolRegistry.GetTool("create_game_object"));
-        Assert.IsNotNull(toolRegistry.GetTool("create_file"));
-        Assert.IsNotNull(toolRegistry.GetTool("modify_file"));
+        Assert.IsNotNull(toolRegistry.GetTool("CreateGameObject"));
+        Assert.IsNotNull(toolRegistry.GetTool("CreateFile"));
+        Assert.IsNotNull(toolRegistry.GetTool("ModifyFile"));
         Assert.IsNotNull(toolRegistry.GetTool("read_file"));
-        Assert.IsNull(toolRegistry.GetTool("delete_file"));
+        Assert.IsNull(toolRegistry.GetTool("DeleteFile"));
     }
 
     [Test]
@@ -38,11 +38,11 @@ public class ToolRegistryTests
         var toolRegistry = new ToolRegistry(settings, "Agent");
 
         // Assert
-        Assert.IsNotNull(toolRegistry.GetTool("create_game_object"));
-        Assert.IsNotNull(toolRegistry.GetTool("create_file"));
-        Assert.IsNotNull(toolRegistry.GetTool("modify_file"));
+        Assert.IsNotNull(toolRegistry.GetTool("CreateGameObject"));
+        Assert.IsNotNull(toolRegistry.GetTool("CreateFile"));
+        Assert.IsNotNull(toolRegistry.GetTool("ModifyFile"));
         Assert.IsNotNull(toolRegistry.GetTool("read_file"));
-        Assert.IsNotNull(toolRegistry.GetTool("delete_file"));
+        Assert.IsNotNull(toolRegistry.GetTool("DeleteFile"));
     }
 
     [Test]
@@ -95,18 +95,27 @@ public class ToolRegistryTests
         // Arrange
         settings.enableDeleteTools = true;
         var toolRegistry = new ToolRegistry(settings, "Agent");
+        var expectedPrompt = "You have access to the following tools. Use them to fulfill the user's request.\n" +
+                             "Tool: CreateGameObject\n" +
+                             "Description: Creates a new GameObject with the given name.\n" +
+                             "Parameters: name (The name of the GameObject to create.)\n\n" +
+                             "Tool: CreateFile\n" +
+                             "Description: Creates a new file with the given content. Useful for creating new C# scripts.\n" +
+                             "Parameters: filePath (The path of the file to create. Should be relative to the Assets folder.), content (The content of the file to create.)\n\n" +
+                             "Tool: ModifyFile\n" +
+                             "Description: Modifies an existing file with a prompt.\n" +
+                             "Parameters: filePath (The path of the file to modify, relative to the Assets folder.), prompt (A prompt describing the modifications to make.)\n\n" +
+                             "Tool: read_file\n" +
+                             "Description: Reads the entire content of a file at the specified path.\n" +
+                             "Parameters: path (The relative path to the file to be read.)\n\n" +
+                             "Tool: DeleteFile\n" +
+                             "Description: Deletes a file at the given path.\n" +
+                             "Parameters: filePath (The path of the file to delete. Should be relative to the Assets folder.)\n\n";
 
         // Act
         var prompt = toolRegistry.GetToolDefinitionsForPrompt();
 
         // Assert
-        StringAssert.Contains("You have access to the following tools.", prompt);
-        StringAssert.Contains("Tool: create_game_object", prompt);
-        StringAssert.Contains("Tool: create_file", prompt);
-        StringAssert.Contains("Tool: modify_file", prompt);
-        StringAssert.Contains("Tool: read_file", prompt);
-        StringAssert.Contains("Tool: delete_file", prompt);
-        StringAssert.Contains("Description:", prompt);
-        StringAssert.Contains("Parameters:", prompt);
+        Assert.AreEqual(expectedPrompt, prompt);
     }
 }
